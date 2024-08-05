48801aaaff16eea99d54d36c00837fa27fb89a65
Atila Fassina <atila@fassina.eu>
2024-08-05 21:49:08 +0200

    remove generated schemas from template

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,25 +10,29 @@ pub fn run() {
     let devtools = tauri_plugin_devtools::init();
     let mut builder = tauri::Builder::default();
 
+    let specta_builder =
+        tauri_specta::Builder::<tauri::Wry>::new().commands(tauri_specta::collect_commands![greet]);
+
     #[cfg(debug_assertions)]
     {
         builder = builder.plugin(devtools);
     }
 
-    let invoke_handler = {
-        let builder = tauri_specta::ts::builder().commands(tauri_specta::collect_commands![greet]);
-
-        #[cfg(all(debug_assertions, not(mobile)))]
-        let builder = builder.path("../src/bindings.ts");
-
-        builder.build().unwrap()
-    };
+    #[cfg(all(debug_assertions, not(mobile)))]
+    specta_builder
+        .export(
+            specta_typescript::Typescript::default()
+                .formatter(specta_typescript::formatter::prettier)
+                .header("/* eslint-disable */\n"),
+            "../src/bindings.ts",
+        )
+        .expect("failed to export typescript bindings");
 
     builder
         .plugin(tauri_plugin_shell::init())
         // .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(invoke_handler)
+        .invoke_handler(specta_builder.invoke_handler())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
