dfce7b6fa020907b99a415d08183f59da2e0983a
Atila Fassina <atila@fassina.eu>
2024-08-05 20:44:36 +0200

    update tauri from beta to rc

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -26,7 +26,7 @@ pub fn run() {
 
     builder
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(invoke_handler)
         .run(tauri::generate_context!())
